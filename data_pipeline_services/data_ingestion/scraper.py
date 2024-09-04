3a904c1d81bfce0c46619810d5ff95f6d32f5885
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-09-04 00:09:52 +0000

    update functions

--- data_pipeline_services/data_ingestion/scraper.py
+++ data_pipeline_services/data_ingestion/scraper.py
@@ -23,7 +23,7 @@ def get_month_links(season):
         - link_text (str): The name of the month (e.g., 'october', 'november').
         - url (str): The full URL to the page for that month.
           
-      If an HTTP error occurs, the function returns (None, None).
+    If an HTTP error occurs, the function returns (None, None).
   """
   try:
     start_year, end_year = season.split('-')
@@ -67,15 +67,15 @@ def get_box_score_links(month_link_list):
   """
   Fetches box score links and corresponding game dates from the given list of month page links.
 
-    Inputs:
-      month_link_list (list of tuples): List of tuples where each tuple contains:
-        - month (str): The name of the month (e.g., 'october').
-        - page (str): The full URL to the page for that month.
+  Inputs:
+    month_link_list (list of tuples): List of tuples where each tuple contains:
+      - month (str): The name of the month (e.g., 'october').
+      - page (str): The full URL to the page for that month.
 
-    Returns:
-      tuple: A tuple containing:
-        - box_link_array (list of lists): A list of lists where each inner list contains the URLs to box scores for the games played in the given month.
-        - all_dates (list of lists): A list of lists where each inner list contains the corresponding dates (formatted as 'YYYYMMDD') for the box scores in the same order as `box_link_array`.
+  Returns:
+    tuple: A tuple containing:
+      - box_link_array (list of lists): A list of lists where each inner list contains the URLs to box scores for the games played in the given month.
+      - all_dates (list of lists): A list of lists where each inner list contains the corresponding dates (formatted as 'YYYYMMDD') for the box scores in the same order as `box_link_array`.
   """
   box_link_array = []
   all_dates = []
@@ -112,17 +112,22 @@ def get_box_score_links(month_link_list):
 
 # TODO UPDATE
 # from https://medium.com/@HeeebsInc/using-machine-learning-to-predict-daily-fantasy-basketball-scores-part-i-811de3c54a98
-def extract_player_data(box_links, all_dates, season):
+def extract_player_data(box_links, all_dates):
   """
   
   """
-  df_columns = ['Date', 'Name', 'Team', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA',
-               '3P%','FT', 'FTA', 'FT%', 'ORB','DRB', 'TRB', 'AST', 'STL', 'BLK', 
-               'TOV', 'PF', 'PTS', 'GmSc', '+-' ]
+  df_columns = [
+                'Date', 'Name', 'Team', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA',
+                '3P%','FT', 'FTA', 'FT%', 'ORB','DRB', 'TRB', 'AST', 'STL', 'BLK', 
+                'TOV', 'PF', 'PTS', 'GmSc', '+-'
+               ]
+  
   stat_df = pd.DataFrame(columns = df_columns)
   error_df = pd.DataFrame(columns = ['URL', 'Error'])
+
   for i, (l, d) in enumerate(zip(box_links, all_dates)):
     print(f'Processing batch {i+1}/{len(box_links)}')
+
     for link, date in zip(l, d):
       print(f'{link}\n{date}')
       print(f'Currently Scraping {link}')
@@ -134,24 +139,24 @@ def extract_player_data(box_links, all_dates, season):
         soup = BeautifulSoup(response.text, 'html.parser')
 
         tables = soup.find_all('table', id=lambda x: x and x.endswith('-game-basic'))
+
         for table in tables:
-          caption = table.find('caption')
-          team_name = caption.text.split(' Basic and Advanced Stats Table')[0].strip()
+          team_name = table.find('caption').text.split(' Basic and Advanced Stats Table')[0].strip()
           rows = table.find('tbody').find_all('tr')
+
           for row in rows:
             if row.find('th').text in ['Team Totals', 'Reserves']:
               continue
             player_name = normalize_name(row.find('th').text.strip())
-            dnp = row.find('td', {'data-stat': 'reason'})
 
             stats = [date, player_name, team_name]
 
+            dnp = row.find('td', {'data-stat': 'reason'})
             if dnp and 'Did Not Play' in dnp.text:
               stats += ['DNP'] * (len(df_columns) - 3)
             else:
               for td in row.find_all('td'):
-                stat = td.text.strip()
-                stats.append(stat if stat else 0)
+                stats.append(td.text.strip() or '0')
   
             if len(stats) == len(df_columns):
               new_row = pd.DataFrame([stats], columns=df_columns)
@@ -159,32 +164,15 @@ def extract_player_data(box_links, all_dates, season):
             else:
               print(f'Skipping incomplete data for {player_name}')
 
-        print(f'Finished Scraping: {link}')
-
       except requests.exceptions.HTTPError as http_err:
-        if response.status_code == 429:                     # TODO Add helper function for handling errors
-          print(f'Rate limit exceeded for {link}, Status code: {response.status_code}')
-          print(f"Retry-After: {response.headers.get('Retry-After')}")
-          time.sleep(int(response.get('Retry-After'), 60))
-        else:
-          print(f'HTTP error occurred: {http_err}')
-        error = {'URL': link, 'Error': f'HTTPError: {http_err}'}
-        new_row = pd.DataFrame([error])
-        error_df = pd.concat([error_df, new_row], ignore_index=True)
+        print(f"HTTP error occurred for {link}: {http_err}")
 
       except Exception as e:                                  # TODO Add helper function for handling errors
-        print(f"Error: {e}")
-        print(f"Exception Type: {type(e).__name__}")
-        error_message = str(e) if str(e) else "No error message provided."
-        print(f'Error Scraping: {link}')
-        error = {'URL': link, 'Error': error_message}
-        new_row = pd.DataFrame([error])
-        error_df = pd.concat([error_df, new_row], ignore_index=True)
+        print(f"General error occurred for {link}: {str(e)}")
       
-      delay = random.uniform(3, 7)
-      print(f"Delaying for {delay:.2f} seconds")
-      time.sleep(delay)
+      time.sleep(random.uniform(3, 7))
 
+  # TODO create function to save the dataframes to CSVs
   stat_df.to_csv(f'Season({season}).csv', lineterminator='\n', index=False)
   error_df.to_csv(f'Errors_Season({season}).csv', lineterminator='\n', index=False)
   
@@ -193,6 +181,7 @@ def extract_player_data(box_links, all_dates, season):
 
       
 def normalize_name(name):
+  """Normalize player names by removing diacritics and converting to lowercase."""
   normalized = unicodedata.normalize('NFKD', name)
   without_diacritics = ''.join(c for c in normalized if not unicodedata.combining(c))
   return without_diacritics.lower()
