dbb6690536b44a06d1986c48dfd5dfd1373801fe
SavviSam <swghazzawi@gmail.com>
2024-09-05 18:01:47 -0600

    init database

---
+++ data_pipeline_services/data_processing/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+
+def load_raw_data(file_path):
+  """
+  Load the raw CSV file into a pandas DataFrame.
+  """
+  df = pd.read_csv(file_path)
+  return df
+
+if __name__ == "__main__":
+  file_path = '../data_ingestion/data/2021-22_Season.csv'
+  raw_df = load_raw_data(file_path)
+  
