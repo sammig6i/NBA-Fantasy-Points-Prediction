5b9e80b131b357ee91cc47983c0584922f04795c
SavviSam <swghazzawi@gmail.com>
2024-09-05 18:25:56 -0600

    update connection to db

--- data_pipeline_services/data_processing/cleaning.py
+++ data_pipeline_services/data_processing/cleaning.py
@@ -1,2 +1,22 @@
+import os
 import pandas as pd
+import psycopg2
+from dotenv import load_dotenv
 
+load_dotenv()
+
+def connect_db():
+  try:
+    conn = psycopg2.connect(
+      user=os.getenv("DB_USER"),
+      password=os.getenv("DB_PASSWORD"),
+      host=os.getenv("DB_HOST"),
+      port=os.getenv("DB_PORT"),
+      database=os.getenv("DB_NAME")
+    )
+    return conn
+  except Exception as err:
+    print(f"Error while connecting to PostgreSQL: {err}")
+    return None
+  
+if 
--- data_pipeline_services/data_processing/main.py
+++ data_pipeline_services/data_processing/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 
-
 def load_raw_data(file_path):
   """
   Load the raw CSV file into a pandas DataFrame.
@@ -12,4 +11,3 @@ def load_raw_data(file_path):
 if __name__ == "__main__":
   file_path = '../data_ingestion/data/2021-22_Season.csv'
   raw_df = load_raw_data(file_path)
-  
