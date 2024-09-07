79cb187a980f7574393cdf568c586bb767219e49
SavviSam <swghazzawi@gmail.com>
2024-09-06 22:52:51 -0600

    fix issues inserting into database

--- data_pipeline_services/data_processing/cleaning.py
+++ data_pipeline_services/data_processing/cleaning.py
@@ -5,6 +5,7 @@ import logging
 from dotenv import load_dotenv
 from psycopg2.extensions import connection
 from data_ingestion.config import TEAM_ABBREVIATIONS
+import hashlib
 
 load_dotenv()
 logging.basicConfig(level=logging.ERROR)
@@ -31,6 +32,7 @@ def load_raw_data(file_path: str) -> pd.DataFrame:
   df = pd.read_csv(file_path)
   return df
 
+
 def convert_team_names_to_abbreviations(df: pd.DataFrame) -> pd.DataFrame:
   """
   Convert full team names to their abbreviations in the DataFrame.
@@ -42,12 +44,14 @@ def convert_team_names_to_abbreviations(df: pd.DataFrame) -> pd.DataFrame:
   
   return df
 
+
 def remove_dnp_and_zero_minutes(df: pd.DataFrame) -> pd.DataFrame:
   """
   Remove rows where players either have 'DNP' in the stats or 0 minutes played (MP == '0:00').
   """
   df = df[~df['MP'].isin(['DNP', '0:00'])]
   return df
+
 
 def convert_mp_to_minutes(df: pd.DataFrame) -> pd.DataFrame:
   """
@@ -56,6 +60,23 @@ def convert_mp_to_minutes(df: pd.DataFrame) -> pd.DataFrame:
   df['MP'] = df['MP'].apply(lambda x: sum(int(t) * 60**i for i, t in enumerate(reversed(x.split(':')))) / 60 if isinstance(x, str) else 0)
   return df
 
+
+def remove_duplicates(df: pd.DataFrame) -> pd.DataFrame:
+  """
+  Remove duplicates from the dataset based on key columns.
+  """
+  df_cleaned = df.drop_duplicates(subset=['Date', 'Team', 'Opponent', 'Name'])
+  return df_cleaned
+
+
+def generate_game_id(game_date: str, team: str, opponent: str) -> str:
+  """
+  Generate a unique ID by hashing combination of game date, home team, and away team
+  """
+  game_key = f"{game_date}_{team}_{opponent}"
+  game_id = hashlib.md5(game_key.encode()).hexdigest()
+  return game_id
+  
 
 def assign_player_ids(df: pd.DataFrame, connection: connection) -> dict:
   """
@@ -83,28 +104,31 @@ def assign_game_ids(df: pd.DataFrame, connection: connection) -> dict:
   """
   Assign unique game IDs to each game and populate the Games table.
   """
-  games = df[['Date', 'Team', 'Opponent', 'GameLink']].drop_duplicates()
+  games = df[['Date', 'Team', 'Opponent', 'Home','GameLink']].drop_duplicates()
   game_id_map = {}
 
   cursor = connection.cursor()
   for index, game in games.iterrows():
     game_date = pd.to_datetime(game['Date'], format='%Y%m%d').strftime('%Y-%m-%d')
-    home_team = game['Team']
-    away_team = game['Opponent']
+    
+    if game['Home'] == 1:
+      home_team = game['Team']
+      away_team = game['Opponent']
+    else:
+      home_team = game['Opponent']
+      away_team = game['Team']
+    
     game_link = game['GameLink']
+    game_id = generate_game_id(game_date, home_team, away_team)
 
     cursor.execute("""
-            INSERT INTO Games (game_date, home_team, away_team, game_link) 
-            VALUES (%s, %s, %s, %s) 
+            INSERT INTO Games (game_id, game_date, home_team, away_team, game_link) 
+            VALUES (%s, %s, %s, %s, %s) 
             ON CONFLICT (game_link) DO NOTHING 
             RETURNING game_id;
-        """, (game_date, home_team, away_team, game_link))
-    result = cursor.fetchone()
-    if result:
-      game_id_map[(game_date, home_team, away_team)] = result[0]
-    else:
-      cursor.execute("SELECT game_id FROM Games WHERE game_link = %s;", (game_link,))
-      game_id_map[(game_date, home_team, away_team)] = cursor.fetchone()[0]
+        """, (game_id, game_date, home_team, away_team, game_link))
+    
+    game_id_map[(game_date, home_team, away_team)] = game_id
 
   connection.commit()
   return game_id_map
@@ -120,9 +144,17 @@ def clean_and_prepare_player_stats(df: pd.DataFrame, player_id_map: dict, game_i
     game_date = pd.to_datetime(row['Date'], format='%Y%m%d').strftime('%Y-%m-%d')
     team = row['Team']
     opponent = row['Opponent']
+    home_status = row['Home']
+
+    if home_status == 1:
+      home_team = team
+      away_team = opponent
+    else:
+      home_team = opponent
+      away_team = team
 
     player_id = player_id_map.get(row['Name'])
-    game_id = game_id_map.get((game_date, team, opponent))
+    game_id = game_id_map.get((game_date, home_team, away_team))
 
     if player_id and game_id:
       cursor.execute("""
@@ -137,45 +169,42 @@ def clean_and_prepare_player_stats(df: pd.DataFrame, player_id_map: dict, game_i
           row['TRB'], row['AST'], row['STL'], row['BLK'], row['TOV'], row['PF'], row['PTS'], row['GmSc'], 
           row['+-']
       ))
-
+s
   connection.commit()
 
 
-def remove_duplicates(df: pd.DataFrame) -> pd.DataFrame:
-  """
-  Remove duplicates from the dataset based on key columns.
-  """
-  df_cleaned = df.drop_duplicates(subset=['Date', 'Team', 'Opponent', 'Name'])
-  return df_cleaned
-
-
 def process_raw_data(file_path: str) -> None:
-  connection = connect_db()
-  if not connection:
-    print("Database connection failed.")
-    return
+  try:
+    # 1 DB connection
+    connection = connect_db()
+    if not connection:
+      print("Database connection failed.")
+      return
 
-  raw_df = load_raw_data(file_path)
+    #2 load raw data
+    raw_df = load_raw_data(file_path)
 
-  cleaned_df = remove_duplicates(raw_df)
-  cleaned_df = convert_team_names_to_abbreviations(cleaned_df)
+    #3 Data cleaning and preprocessing
+    df = remove_duplicates(raw_df)
+    df = convert_team_names_to_abbreviations(df)
+    df = remove_dnp_and_zero_minutes(df)
+    df = convert_mp_to_minutes(df)
 
-  player_id_map = assign_player_ids(cleaned_df, connection)
-  game_id_map = assign_game_ids(cleaned_df, connection)
+    #4 assign unique IDs for players and games
+    player_id_map = assign_player_ids(df, connection)
+    game_id_map = assign_game_ids(df, connection)
 
-  clean_and_prepare_player_stats(cleaned_df, player_id_map, game_id_map, connection)
-
-  connection.close()
+    #5 Insert cleaned player stats into database
+    clean_and_prepare_player_stats(df, player_id_map, game_id_map, connection)
+  except Exception as e:
+    print(f"Error processing raw data: {e}")
+  finally:
+    connection.close()
 
 
 #! Validate columns to be correct data types '+-' -> positive/negative int, 'GmSc' -> positive/negative float
-# TODO function to convert 'MP' to represent time as a float
 
 if __name__ == "__main__":
   file_path = os.path.join(os.path.dirname(__file__), '../data_ingestion/data/2021-22_Season.csv')
-  # process_raw_data(file_path)
-  raw_df = load_raw_data(file_path)
-  df = remove_duplicates(raw_df)
-  df = remove_dnp_and_zero_minutes(df)
-  cleaned_df = convert_mp_to_minutes(df)
-  print(cleaned_df['MP'])
+  process_raw_data(file_path)
+  
